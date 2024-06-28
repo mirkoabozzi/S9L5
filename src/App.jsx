b8337562038fb55afd8a945c38c6786721b5febf
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-06-28 11:55:34 +0200

    refactor react home + footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MyNav from "./components/MyNav";
 import Home from "./components/Home";
+import MyFooter from "./components/MyFooter";
 
 function App() {
   return (
     <div className="">
       <MyNav />
       <Home />
+      <MyFooter />
     </div>
   );
 }
